6634a34ec8ac1fb6b4e75f0679a484f8ee888630
jonassodergren <jonas.sodergren@gmail.com>
2020-02-17 10:55:18 +0100

    First version

---
+++ script.js
@@ -0,0 +1,113 @@
+console.clear();
+var trace = console.log.bind(console);
+var listSelector = "#question-list-container li";
+
+
+
+//var idx;
+//$.getJSON("index.json", function(json) {
+//  console.log("start loading index");
+//  idx = lunr.Index.load(JSON.parse(json));
+//  console.log("finished loading index");
+//});
+
+//var idx = lunr(function() {
+  // define searchable fields
+//  this.ref("id");
+//  this.field("title");
+//  this.field("body");
+
+//  this.k1(1.3)
+//  this.b(0)
+
+  // create list of all searchable entries by reading out the
+  // '#question-list-container' and saving it as a list of json objects
+  // in this case: {id: , title: , ref: }
+//  var documents = htmlElementsToJSON(listSelector, function($element) {
+//    var ref = $element.attr("data-question-id"),
+//      title = $element.find("h2 a").text(),
+//      body = $element.find("p").text();
+
+//    return { id: ref, title: title, body: body };
+//  });
+
+
+  // adding all entires to lunr
+//  documents.forEach(function(doc) {
+//    this.add(doc);
+//  }, this);
+//});
+
+
+
+var idx;
+$.getJSON("index.json", function(json) {
+  console.log(json);
+  idx = lunr.Index.load(json);
+});
+
+
+//
+function htmlElementsToJSON(listSelector, unmarschallFunction) {
+  // add the list elements to lunr
+  var qs = $(listSelector);
+  var entries = [];
+  for (var i = 0; i < qs.length; i++) {
+    var $q = $(qs[i]);
+    entries.push(unmarschallFunction($q));
+  }
+  return entries;
+}
+
+function search(searchTerm) {
+  var results = idx.search("title:"+searchTerm);
+
+  // reset(hide) all entries
+  $(listSelector).removeClass("show");
+
+  for (var i = 0; i < results.length; i++) {
+    var result = results[i];
+    $(listSelector + "[data-question-id=" + result.ref + "]").addClass("show");
+  }
+}
+
+function showAll(searchTerm) {
+  $(listSelector).addClass("show");
+}
+
+//$("#searchterm").on("search paste keyup", function(event) {
+//  var st = $(this).val();
+//
+//  if (st.length === 0) {
+//    showAll();
+//  } else {
+//    // make it async, otherwise the keyboard input is interrupted
+//    setTimeout(function() {
+//      search(st);
+//    }, 100);
+//  }
+//});
+
+//$("#searchterm").on("keydown", function(event) {
+//  var st = $(this).val();
+//
+//  if(event.which == 13)
+//    search(st);
+//
+//});
+
+// and show all results when clicking this button
+$(".all").click(function() {
+  showAll();
+});
+
+//$("#search2").click(function() {
+//  search($("#searchterm").val());
+//});
+
+$("#search2").click(function(event) {
+    search($("#searchterm").val());
+  //  btnMostrarSeccion($(this));
+    event.preventDefault();
+//    search($("#searchterm").val());
+})
